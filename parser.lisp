fee3ca57c68a9ba5a71f910ab69acf84f777b947
kchen29 <kchen29@stuy.edu>
2018-06-26 21:01:22 -0400

    parsing

--- parser.lisp
+++ parser.lisp
@@ -1,27 +1,34 @@
-;;;;Parse HTML using reader macros
+;;;; Parse HTML using reader macros
 
-;;;not all of them
+;;; not all of them
 (defvar void-tags '(br hr))
 
 (defun |<-reader| (stream char)
   (declare (ignore char))
-  (let ((tag (read-delimited-list #\> stream t)))
-    (if (or (closing-tag-p tag) (void-tag-p tag))
+  (let* ((tag (read-delimited-list #\> stream t))
+         (name (car tag)))
+    (if (or (closing-tag-p name) (void-tag-p name))
         tag
-        (cons (car tag)
-              (loop with closing = (list (concatenate 'string "/"
-                                                      (symbol-name (car tag))))
+        (cons name
+              (loop with closing = (concatenate 'string "/"
+                                                (symbol-name name))
                     for next = (read stream nil nil t)
-                    if (equal next closing)
+                    if (string= (string (car next)) closing)
                       return output
                     else
                       collect next into output)))))
 
-(defun closing-tag-p (tag)
-  (char= #\/ (char (string (car tag)) 0)))
+(defun closing-tag-p (name)
+  (char= #\/ (char (string name) 0)))
 
-(defun void-tag-p (tag)
-  (member tag void-tags))
+(defun void-tag-p (name)
+  (member name void-tags))
 
-(set-macro-character #\> nil nil)
+(set-macro-character #\> nil nil)        ; make sure > doesn't get parsed as part of a symbol
 (set-macro-character #\< #'|<-reader|)
+
+;;; test out reader macro
+(defmacro html (&whole whole &rest rest)
+  (declare (ignore rest))
+  (print whole)
+  nil)
